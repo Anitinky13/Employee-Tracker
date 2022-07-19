c9711342118ae6c520a294f9081176402643a82b
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-07-19 17:24:13 -0500

    fixed schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,30 @@
--DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employee_db;
+-DROP DATABASE IF EXISTS employeesData
+CREATE DATABASE employeesData;
 
--- USE employee_db;
+ USE employeesData;
+
+
 CREATE TABLE department (
-  id INT  AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  id INT  AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) 
+
 );
 
 CREATE TABLE role (
-  id INT  AUTO_INCREMENT, PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL(7.2) NOT NULL,
+  id INT  AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
   department_id INT
-CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id)
+CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(department_id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NULL,
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30)
+  last_name VARCHAR(30) 
+  role_id INT,
   manager_id INT NULL,
   PRIMARY KEY (id)
-  CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES  role_id(id)
-     FOREIGN KEY(manager_id) REFERENCES  employee(id)
+  CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES  role(role_id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY(manager_id) REFERENCES  employee(employee_id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -6,4 +6,19 @@ VALUES
 ('Sales'),
 ('Legal');
 
-INSERT INTO role()
+INSERT INTO role(title, salary, department_id)
+VALUES
+('IT Engineer', 80000, 1),
+('Network Engineer', 90000, 1),
+('System Engineer', 95000, 1),
+('Technical Support Engineer',60000, 1),
+('Equity Analyst', 100000, 2),
+('Investment Banker', 150000, 2),
+('Tax Director', 200000, 2),
+('Vice President of Analytics', 200000, 2),
+('Account Manager', 80000, 3),
+('Account Executive', 100000, 3),
+('Sales Director', 200000, 3),
+('Legal Tech', 90000, 4),
+('Trial Lawyers', 100000, 4),
+('Tax Attorney', 100000, 4);
